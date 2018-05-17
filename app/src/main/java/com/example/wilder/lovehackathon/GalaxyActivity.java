b1d782155d82a1ea9655ee148515073520d2d5f8
Clairehere <b_claire@hotmail.com>
2018-05-17 20:44:56 +0200

    action click planete

--- app/src/main/java/com/example/wilder/lovehackathon/GalaxyActivity.java
+++ app/src/main/java/com/example/wilder/lovehackathon/GalaxyActivity.java
@@ -2,7 +2,10 @@ package com.example.wilder.lovehackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,15 +27,11 @@ public class GalaxyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galaxy);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference profilRef = database.getReference();
-
-
-        ListView listPlanete1 = findViewById(R.id.lv1);
-        ListView listPlanete2 = findViewById(R.id.lv2);
-        ListView listPlanete3 = findViewById(R.id.lv3);
-        ListView listPlanete4 = findViewById(R.id.lv4);
-        ListView listPlanete5 = findViewById(R.id.lv5);
+        final ListView listPlanete1 = findViewById(R.id.lv1);
+        final ListView listPlanete2 = findViewById(R.id.lv2);
+        final ListView listPlanete3 = findViewById(R.id.lv3);
+        final ListView listPlanete4 = findViewById(R.id.lv4);
+        final ListView listPlanete5 = findViewById(R.id.lv5);
 
         final PopulationAdapteur adapter1 = new PopulationAdapteur(GalaxyActivity.this, mList1);
         PopulationAdapteur adapter2 = new PopulationAdapteur(GalaxyActivity.this, mList2);
@@ -67,6 +66,59 @@ public class GalaxyActivity extends AppCompatActivity {
         mList5.add(new ProfilModel(null, null, null, null, null, null, "Thomas", null, null));
         mList5.add(new ProfilModel(null, null, null, null, null, null, "Hedie", null, null));
         listPlanete5.setAdapter(adapter5);
+
+
+        ImageView planete1 = findViewById(R.id.iv_planete1);
+        ImageView planete2 = findViewById(R.id.iv_planete2);
+        ImageView planete3 = findViewById(R.id.iv_planete3);
+        ImageView planete4 = findViewById(R.id.iv_planete4);
+        ImageView planete5 = findViewById(R.id.iv_planete5);
+
+        final TextView pop1 = findViewById(R.id.tv_population1);
+        final TextView pop2 = findViewById(R.id.tv_population2);
+        final TextView pop3 = findViewById(R.id.tv_population3);
+        final TextView pop4 = findViewById(R.id.tv_population4);
+        final TextView pop5 = findViewById(R.id.tv_population5);
+
+        planete1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPlanete1.setVisibility(View.VISIBLE);
+                pop1.setVisibility(View.VISIBLE);
+            }
+        });
+
+        planete2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPlanete2.setVisibility(View.VISIBLE);
+                pop2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        planete3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPlanete3.setVisibility(View.VISIBLE);
+                pop3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        planete4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPlanete4.setVisibility(View.VISIBLE);
+                pop4.setVisibility(View.VISIBLE);
+            }
+        });
+
+        planete5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPlanete5.setVisibility(View.VISIBLE);
+                pop5.setVisibility(View.VISIBLE);
+            }
+        });
 
 
 
